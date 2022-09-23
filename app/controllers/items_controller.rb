f7c6b3d74d7c272a677db8128771773b09f923a5
Emma Cohanim <emmacohanim@gmail.com>
2022-09-23 10:19:28 -0700

    commit tree 4c0364d1edd25722d33601b1b1915c9fc993e11d

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id] # if you got here using nested route
+      # only want set of items that belong to specific user
+      items = User.find(params[:user_id]).items
+      render json: items
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
+
+  def show
+    # if you want specific item for specific user
+    item = User.find(params[:user_id]).items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = User.find(params[:user_id]).items.create!(item_params)
+    render json: item, status: :created
+  end
+
+private
+
+def render_not_found_response
+  render json: {error: 'Record not found'}, status: :not_found
+end
+
+def item_params
+  params.permit(:name, :description, :price)
+end
+
+def invalid
+  render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+end
 
 end
